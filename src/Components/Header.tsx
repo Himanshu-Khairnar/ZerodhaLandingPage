cb4eb1b80bbb85a157119a6540dc5b0d8dce28e2
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-01-20 22:25:12 +0530

    Reintialize the project

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,58 +1,37 @@
-import { Menu } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Menu } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-
-const links = [
-  {
-    name: "Signup",
-    href: "/login",
-  },
-  {
-    name: "About",
-    href: "/about",
-  },
-  {
-    name: "Products",
-    href: "/products",
-  },
-  {
-    name: "Pricing",
-    href: "/pricing",
-  },
-  {
-    name: "Support",
-    href: "/support",
-  },
-]
 export default function Header() {
-  return (
-    <div className='px-32 w-full gap-28  flex justify-between items-center    shadow '>
-      <div>
-        <Image src={'/logo.svg'} width={100} height={100} className='w-32 text-[16px]' alt='logo' />
+    return (
+        <div className="flex items-center justify-between w-full px-[14%] py-[1.5%] text-[#666666]  shadow">
+            
+            <div className="flex items-center">
+                <Image src="/logo.svg" alt="logo" width={100} height={100} className="w-[120px]" />
+            </div>
 
-      </div>
+            <div className="hidden md:flex gap-[40px] text-[14.4px]">
+                <Link href="/" className="hover:text-blue-500">
+                    Signup
+                </Link>
+                <Link href="/" className="hover:text-blue-500">
+                    About
+                </Link>
+                <Link href="/" className="hover:text-blue-500">
+                    Products
+                </Link>
+                <Link href="/" className="hover:text-blue-500">
+                    Pricing
+                </Link>
+                <Link href="/" className="hover:text-blue-500">
+                    Support
+                </Link>
+                <Menu className="w-6 h-6 text-[#666666] hover:text-blue-500" />
+            </div>
 
-      <div className='flex items-center text-[14.4px] gap-2 pl-32' >
-       
-        {
-          links.map((link) => {
-            return (
-              <Link href={link.href} className='mx-3 hover:text-[#387ED1]'>{link.name}</Link>
-            )
-          })
-          
-        }
-        <div className='gap-3'>
-          <Menu/>
+            <div className="flex md:hidden">
+            </div>
         </div>
-
-      </div>
-      <div>
-
-      </div>
-    </div>
-
-  )
+    );
 }
